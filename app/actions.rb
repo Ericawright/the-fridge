834d491dcbf51aa5a1758e3ff550709a86992a8a
Erica Wright <ericaewright@gmail.com>
2015-04-24 20:18:27 +0000

    helper method, fix route

--- app/actions.rb
+++ app/actions.rb
@@ -2,14 +2,10 @@ enable :sessions
 
 helpers do
   def current_user
-    begin
-      @current_user = User.find(session["user_id"].to_i)
-      rescue
-        @current_user = nil
-      end
-    end
+    @current_user ||= User.find(session[:user_id])
   end
-  
+end
+
 get '/' do
   erb :index
 end
@@ -33,11 +29,13 @@ post '/signup' do
     erb :'user/new'
   end
 
-  post '/login' do
-    @current_user = User.where(username: params[:username], password:  params[:password]).first
-    session[:user_id] = @current_user.id
-    redirect '/'
-  end
-  
 end
+
+post '/login' do
+  @current_user = User.where(username: params[:username], password:  params[:password]).first
+  session[:user_id] = @current_user.id
+  redirect '/'
+end
+  
+
 
